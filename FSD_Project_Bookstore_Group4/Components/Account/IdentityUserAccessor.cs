62e0b82699444822c1f33a8e835f8e9c16ac6f57
Js-wn <2302420A@student.tp.edu.sg>
2024-11-29 09:20:34 +0800

    Blazor identity added

---
+++ FSD_Project_Bookstore_Group4/Components/Account/IdentityUserAccessor.cs
@@ -0,0 +1,20 @@
+﻿using FSD_Project_Bookstore_Group4.Data;
+using Microsoft.AspNetCore.Identity;
+
+namespace FSD_Project_Bookstore_Group4.Components.Account
+{
+    internal sealed class IdentityUserAccessor(UserManager<FSD_Project_Bookstore_Group4User> userManager, IdentityRedirectManager redirectManager)
+    {
+        public async Task<FSD_Project_Bookstore_Group4User> GetRequiredUserAsync(HttpContext context)
+        {
+            var user = await userManager.GetUserAsync(context.User);
+
+            if (user is null)
+            {
+                redirectManager.RedirectToWithStatus("Account/InvalidUser", $"Error: Unable to load user with ID '{userManager.GetUserId(context.User)}'.", context);
+            }
+
+            return user;
+        }
+    }
+}
--- FSD_Project_Bookstore_Group4/Program.cs
+++ FSD_Project_Bookstore_Group4/Program.cs
@@ -1,10 +1,40 @@
-using FSD_Project_Bookstore_Group4.Components;
+﻿using FSD_Project_Bookstore_Group4.Components;
+using FSD_Project_Bookstore_Group4.Components.Account;
+using FSD_Project_Bookstore_Group4.Data;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("IdentityContext") ?? throw new InvalidOperationException("Connection string 'IdentityContextConnection' not found.");;
+
+builder.Services.AddDbContext<IdentityContext>(options => options.UseSqlServer(connectionString));
 
 // Add services to the container. 
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.AddCascadingAuthenticationState();
+
+builder.Services.AddScoped<IdentityUserAccessor>();
+
+builder.Services.AddScoped<IdentityRedirectManager>();
+
+builder.Services.AddScoped<AuthenticationStateProvider, IdentityRevalidatingAuthenticationStateProvider>();
+
+builder.Services.AddAuthentication(options =>
+    {
+        options.DefaultScheme = IdentityConstants.ApplicationScheme;
+        options.DefaultSignInScheme = IdentityConstants.ExternalScheme;
+    })
+    .AddIdentityCookies();
+
+builder.Services.AddIdentityCore<FSD_Project_Bookstore_Group4User>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<IdentityContext>()
+    .AddSignInManager()
+    .AddDefaultTokenProviders();
+
+builder.Services.AddSingleton<IEmailSender<FSD_Project_Bookstore_Group4User>, IdentityNoOpEmailSender>();
 
 var app = builder.Build();
 
@@ -23,5 +53,7 @@ app.UseAntiforgery();
 
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
+
+app.MapAdditionalIdentityEndpoints();;
 
 app.Run();
