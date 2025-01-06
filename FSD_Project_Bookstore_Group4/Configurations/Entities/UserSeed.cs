b21cf60a7c12e27d85490710cc12c5c94fb42350
Js-wn <2302420A@student.tp.edu.sg>
2025-01-06 09:03:28 +0800

    Added UserSeed UserRoleSeed RoleSeed

---
+++ FSD_Project_Bookstore_Group4/Configurations/Entities/UserSeed.cs
@@ -0,0 +1,28 @@
+﻿using FSD_Project_Bookstore_Group4.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+namespace FSD_Project_Bookstore_Group4.Configurations.Entities
+{
+    public class UserSeed : IEntityTypeConfiguration<FSD_Project_Bookstore_Group4User>
+    {
+        public void Configure(EntityTypeBuilder<FSD_Project_Bookstore_Group4User> builder)
+        {
+            var hasher = new PasswordHasher<FSD_Project_Bookstore_Group4User>();
+            builder.HasData(
+            new FSD_Project_Bookstore_Group4User
+            {
+                Id = "3781efa7-66dc-47f0-860f-e506d04102e4",
+                Email = "admin@localhost.com",
+                NormalizedEmail = "ADMIN@LOCALHOST.COM",
+                FirstName = "Admin",
+                LastName = "User",
+                UserName = "admin@localhost.com",
+                NormalizedUserName = "ADMIN@LOCALHOST.COM",
+                PasswordHash = hasher.HashPassword(null, "P@ssword1"),
+                EmailConfirmed = true // Set to true, otherwise you won't be able to login
+            }
+            );
+        }
+    }
+}
--- FSD_Project_Bookstore_Group4/Data/FSD_Project_Bookstore_Group4User.cs
+++ FSD_Project_Bookstore_Group4/Data/FSD_Project_Bookstore_Group4User.cs
@@ -5,5 +5,7 @@ namespace FSD_Project_Bookstore_Group4.Data
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class FSD_Project_Bookstore_Group4User : IdentityUser
     {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
     }
 }
