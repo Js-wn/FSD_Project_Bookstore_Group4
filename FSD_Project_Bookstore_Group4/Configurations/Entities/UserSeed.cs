e82a29c4ea7e8f3636b539fb0ff63c6dffcda8dd
Js-wn <2302420A@student.tp.edu.sg>
2025-01-10 09:59:48 +0800

    UserSeed

--- FSD_Project_Bookstore_Group4/Configurations/Entities/UserSeed.cs
+++ FSD_Project_Bookstore_Group4/Configurations/Entities/UserSeed.cs
@@ -21,6 +21,18 @@ namespace FSD_Project_Bookstore_Group4.Configurations.Entities
                 NormalizedUserName = "ADMIN@LOCALHOST.COM",
                 PasswordHash = hasher.HashPassword(null, "P@ssword1"),
                 EmailConfirmed = true // Set to true, otherwise you won't be able to login
+            },
+            new FSD_Project_Bookstore_Group4User
+            {
+                Id = "5436aeb6-34ac-90f0-860f-e446d23252e4",
+                Email = "customer@localhost.com",
+                NormalizedEmail = "CUSTOMER@LOCALHOST.COM",
+                FirstName = "Customer",
+                LastName = "User",
+                UserName = "customer@localhost.com",
+                NormalizedUserName = "CUSTOMER@LOCALHOST.COM",
+                PasswordHash = hasher.HashPassword(null, "P@ssword1"),
+                EmailConfirmed = true // Set to true, otherwise you won't be able to login
             }
             );
         }
